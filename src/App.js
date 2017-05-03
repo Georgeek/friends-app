848d849e72dc8b896be1abb8735942713bb98329
georgeek <egormad@gmail.com>
2017-05-03 23:55:25 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col, Button } from "react-bootstrap";
 
 import Friend from './components/friends';
 
@@ -9,23 +11,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              Testing friends app
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to My React</h2>
         </div>
 
         <div className="friend-list">
-          <Friend
-            name={'Georgeek'}
-            img={'https://pp.userapi.com/c626622/v626622756/6d4c1/KpTzKg6lde0.jpg'}/>
-          <Friend
-            name={'Busya'}
-            img={'https://pp.userapi.com/c630220/v630220870/49b1d/slL4JTst23o.jpg'}
-          />
-          <Friend
-            name={'Peche'}
-            img={'https://pp.userapi.com/c630918/v630918756/3ec43/ZgrOiAXsIv0.jpg'}
-          />
+          <Grid>
+            <Friend
+              name={'Georgeek'}
+              img={'https://pp.userapi.com/c626622/v626622756/6d4c1/KpTzKg6lde0.jpg'}/>
+            <Friend
+              name={'Businka'}
+              img={'https://pp.userapi.com/c630220/v630220870/49b1d/slL4JTst23o.jpg'}
+            />
+            <Friend
+              name={'Peche'}
+              img={'https://pp.userapi.com/c630918/v630918756/3ec43/ZgrOiAXsIv0.jpg'}
+            />
+            <Friend
+              name={'Chebusya'}
+              img={'https://pp.userapi.com/c630918/v630918756/3ec43/ZgrOiAXsIv0.jpg'}
+            />
+
+          </Grid>
         </div>
 
       </div>
--- src/components/friends.jsx
+++ src/components/friends.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Grid, Row, Col, Button, Image } from "react-bootstrap";
 
 function Friend(props) {
     return (
-        <div className="friend--wrap">
-            <div className="friend--photo">
-                <img className="" src={props.img} alt=""/>
-            </div>
-            <div className="friend--info">
-                <div className="friend--info__title">hello {props.name}</div>
-                <div className="friend--info__description">large awesome text about this good person for science!! Awesome description about {props.name}</div>
-                <button className="some--button">Написать сообщение {props.name}</button>
-            </div>
-        </div>
+        <Row>
+            <Col md={4} mdOffset={6}>
+                <div className="friend--wrap">
+                    <div className="friend--photo">
+                        <Image src={props.img} alt="" circle />
+                    </div>
+                    <div className="friend--info">
+                        <div className="friend--info__title">hello {props.name}</div>
+                        <div className="friend--info__description">large awesome text about this good person for science!! Awesome description about {props.name}</div>
+                    </div>
+                    <Button bsStyle="primary">Friend</Button>
+                </div>
+            </Col>
+        </Row>
     );
 }
 
